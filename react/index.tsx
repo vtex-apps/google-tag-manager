542c4f13ba34a4f877da71e64e819cd06cad303f
Breno Calazans <brenocalazans@gmail.com>
2019-05-07 17:41:57 -0300

    Remove eval

--- react/index.tsx
+++ react/index.tsx
@@ -27,8 +27,14 @@ if (!gtmId) {
   throw new Error('Warning: No Google Tag Manager ID is defined. To setup this app, take a look at your admin')
 }
 
-// tslint:disable-next-line no-eval
-eval(gtmScript(window.__SETTINGS__.gtmId))
+// GTM script snippet. Taken from: https://developers.google.com/tag-manager/quickstart
+/* tslint:disable */
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',gtmId)
+/* tslint:enable */
 
 window.dataLayer = window.dataLayer || []
 
