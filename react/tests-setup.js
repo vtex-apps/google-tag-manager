1e79b85749f83382866b54ee769845b0cf7b0f05
Breno Calazans <brenocalazans@gmail.com>
2019-05-22 18:52:50 -0300

    Use vtex-test-tools

--- react/tests-setup.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-window.matchMedia = window.matchMedia || function() {
-  return {
-    matches: false,
-    addListener: function() {},
-    removeListener: function() {},
-  }
-}
