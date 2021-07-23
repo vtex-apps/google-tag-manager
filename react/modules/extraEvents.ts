6f9796f415bbd55a6d78fe39f06711b3b3abd53f
Lucian Fialho <lucianfialhobp@hotmail.com>
2021-07-23 14:48:15 -0300

    -- add pageCategory for contentGroup on Google Analytics

--- react/modules/extraEvents.ts
+++ react/modules/extraEvents.ts
@@ -8,6 +8,7 @@ export async function sendExtraEvents(e: PixelMessage) {
         event: 'pageView',
         location: e.data.pageUrl,
         page: e.data.pageUrl.replace(e.origin, ''),
+        pageCategory: e.data.routeId,
         referrer: e.data.referrer,
         ...(e.data.pageTitle && {
           title: e.data.pageTitle,
