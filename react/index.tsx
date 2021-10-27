2390b448bab8c2191d415d96ce6be2a30a94355c
Ícaro <icaro.azevedo@vtex.com.br>
2021-10-27 13:35:44 -0300

    Adding original variables to all events and adding expiration logic

--- react/index.tsx
+++ react/index.tsx
@@ -1,11 +1,14 @@
 import { canUseDOM } from 'vtex.render-runtime'
+
 import { sendEnhancedEcommerceEvents } from './modules/enhancedEcommerceEvents'
 import { sendExtraEvents } from './modules/extraEvents'
 import { sendLegacyEvents } from './modules/legacyEvents'
 import { PixelMessage } from './typings/events'
 
 // no-op for extension point
-export default function () { return null }
+export default function() {
+  return null
+}
 
 export function handleEvents(e: PixelMessage) {
   sendEnhancedEcommerceEvents(e)
