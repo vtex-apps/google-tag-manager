b025c618a65195b5b3f73a528425567ef5783c11
rcapeto <raphaelcapeto@gmail.com>
2022-02-09 10:43:11 -0300

    reset ecommerce key in data object [dataLayer]

--- react/modules/updateEcommerce.ts
+++ react/modules/updateEcommerce.ts
@@ -1,9 +1,17 @@
 import push from './push';
 
-export default function updateEcommerce(eventName: string, data: Record<string, unknown>) {
+export default function updateEcommerce(
+   eventName: string, 
+   data: Record<string, unknown>
+) {
    const eventIndex = window.dataLayer.findIndex(gtmEvent => gtmEvent.event === eventName);
-
    (eventIndex >= 0) && window.dataLayer.splice(eventIndex, 1);
+
+   if(Object.keys(data).includes('ecommerce')) {
+      const ecommerceNullIndex = window.dataLayer.findIndex(gtmEvent => gtmEvent.ecommerce === null);
+      (ecommerceNullIndex >= 0) && window.dataLayer.splice(ecommerceNullIndex, 1);
+      push({ ecommerce: null });
+   }
    
    push(data);
 }
