9959e7ac9bf276d383c1a358c51d07f0a211c65d
Lucas Feijó <lucas.portela@vtex.com.br>
2023-02-08 17:51:05 -0300

    fix: event data

--- react/modules/gaEvents.ts
+++ react/modules/gaEvents.ts
@@ -1,4 +1,4 @@
-import { PixelMessage, CartItem } from '../typings/events'
+import { PixelMessage } from '../typings/events'
 import updateEcommerce from './updateEcommerce'
 import {
   getPrice,
@@ -124,33 +124,28 @@ export function addToCart(eventData: PixelMessage['data']) {
   if (!shouldMergeUAEvents()) return
 
   const eventName = 'add_to_cart'
-  const { currency, items } = eventData
 
-  const formattedPrices = items.map((item: CartItem) =>
+  const {
+    currency,
+    items: [item],
+  } = eventData
+
+  const productName = getProductNameWithoutVariant(item.name, item.skuName)
+  const formattedPrice =
     item.priceIsInt === true ? item.price / 100 : item.price
-  )
-
-  const totalValue = formattedPrices.reduce(
-    (accumulator: number, currentValue: number) => accumulator + currentValue,
-    0
-  )
 
   const data = {
     currency,
-    value: totalValue,
-    items: items.map((item: CartItem) => {
-      const productName = getProductNameWithoutVariant(item.name, item.skuName)
-
-      return {
-        item_id: item.productId,
-        item_brand: item.brand,
-        item_name: productName,
-        item_variant: item.skuId,
-        item_category: item.category,
-        quantity: item.quantity,
-        price: item.priceIsInt === true ? item.price / 100 : item.price,
-      }
-    }),
+    value: formattedPrice,
+    items: {
+      item_id: item.productId,
+      item_brand: item.brand,
+      item_name: productName,
+      item_variant: item.skuId,
+      item_category: item.category,
+      quantity: item.quantity,
+      price: formattedPrice,
+    },
   }
 
   updateEcommerce(eventName, data)
